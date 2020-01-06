019af92cd9d71b9e778f39ef5bcfa807ab66c01a
emuskat <emuskat@mail.yu.edu>
2020-01-06 10:34:17 -0500

    fixed changed

--- testing.bash
+++ testing.bash
@@ -1,12 +1,12 @@
 # clear the contents of the results files
-echo -n ""> finalResults1.txt
-echo -n ""> finalResults2.txt
+echo -n ""> ./finalResults1.txt
+echo -n ""> ./finalResults2.txt
 
 #run the pipe line version of printing out the top ten names and save results into finalResults1.txt
-cat /data/raw/census/dist.all.last |head -10>> finalResults1.txt
+cat /data/raw/census/dist.all.last |head -10>> ./finalResults1.txt
 
 #run the code with the popen version of finding the top ten names and save the results into finalResults.txt
-./finalProj >> finalResults2.txt
+./finalProj >> ./finalResults2.txt
 
-diff finalResults1.txt finalResult2.txt
+diff finalResults1.txt finalResults2.txt
    
