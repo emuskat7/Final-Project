ba4865dc25011009c89aba872335b351260b6e22
emuskat <emuskat@mail.yu.edu>
2020-01-06 10:33:06 -0500

    added error checking

--- finalProj.c
+++ finalProj.c
@@ -6,15 +6,33 @@ int main()
     FILE *fp;
     int PATH_MAX = 100;
     char path[PATH_MAX];
+
     //use popen to open a stream and execute the first command of my two piped commands
-    //open the file containing the census of all the names and their frequencies 
+    //open the file containing the census of all the names and their frequencies
     fp = popen( "cat /data/raw/census/dist.all.last", "r");
+
+    //handle error with popen
+     if  (fp == NULL){
+     printf("there was an error if with popen");
+     return -1;
+    }
+    
     //fp now has that output stream
     //print the first ten lines of the stream using a for loop - simulating piping the file ouput into the head -10 command
     for(int i=0; i<=9; i++){
-      fgets(path, PATH_MAX, fp);
-      printf("%s", path);
+      //check to make sre there are ten names
+      if (fgets(path, PATH_MAX, fp) != NULL)
+	printf("%s", path);
+       else {
+	printf("there were not 10 lines in the output stream from the file");
+	return -1;
+	}
     }
     //after printing out the top ten names from the cesnsus file use pclose to close the stream that was opened by popen
-    pclose(fp);
+    //check to make sure the pclose had no error
+    int status = pclose(fp);
+    if (status == -1){
+    printf("there was an error with pclose");
+    return -1;
+    }
   }
