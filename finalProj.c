47ac395a528eb54832f95609a2117e7de48ff664
emuskat <emuskat@mail.yu.edu>
2020-01-08 09:50:12 -0500

    small fixes

--- finalProj.c
+++ finalProj.c
@@ -1,38 +1,50 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int main()
   {
     //declare the variables that will be used
-    FILE *fp;
-    int PATH_MAX = 100;
-    char path[PATH_MAX];
+    FILE *streamOne;
+    FILE *streamTwo;
+    char result[1000];
+    int statusOne, statusTwo;
 
     //use popen to open a stream and execute the first command of my two piped commands
     //open the file containing the census of all the names and their frequencies
-    fp = popen( "cat /data/raw/census/dist.all.last", "r");
+    streamOne = popen( "cat /data/raw/census/dist.all.last", "r");
 
-    //handle error with popen
-     if  (fp == NULL){
-     printf("there was an error if with popen");
-     return -1;
+     //handle error with popen for streamOne 
+     if  (streamOne == NULL){
+     perror("Error with the popen of streamOne");
+     exit(EXIT_FAILURE);
     }
-    
-    //fp now has that output stream
-    //print the first ten lines of the stream using a for loop - simulating piping the file ouput into the head -10 command
-    for(int i=0; i<=9; i++){
-      //check to make sre there are ten names
-      if (fgets(path, PATH_MAX, fp) != NULL)
-	printf("%s", path);
-       else {
-	printf("there were not 10 lines in the output stream from the file");
-	return -1;
-	}
-    }
-    //after printing out the top ten names from the cesnsus file use pclose to close the stream that was opened by popen
-    //check to make sure the pclose had no error
-    int status = pclose(fp);
-    if (status == -1){
-    printf("there was an error with pclose");
-    return -1;
-    }
+
+     //use popen to take the resulting of the first popen and take the first ten lines and read them into a new file 
+     streamTwo = popen("head -10" , "w");
+
+     //handle error with popen for streamTwo
+      if  (streamTwo == NULL){
+	 perror("Error with the popen of streamTwo");
+	 exit(EXIT_FAILURE);
+      }
+
+     //act as the pipe between streamOne and streamTwo, reading in streamOne and printing out the result to streamTwo
+     while (fgets(result, sizeof(result), streamOne)!=NULL){
+      fprintf(streamTwo, "%s", result);
+      }
+
+     //print the result of streamTwo thus simulating the pipe command which also prints the result of the command if not redirected
+      while (fgets(result, sizeof(result), streamTwo)!=NULL){
+	printf("%s", result);
+      }
+
+     
+     //execute pclose to close the streams and do error checking to make sure they closed properly
+     statusOne = pclose(streamOne);
+     statusTwo = pclose(streamTwo);
+     if (statusOne == -1|| statusTwo == -1)
+       exit(EXIT_FAILURE);
+     
+
+     return 0;
   }
+    
